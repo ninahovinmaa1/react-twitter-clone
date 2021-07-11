254b1ff9c81a6d17bef81ee87e30b3e0205e4565
Nina <>
2021-07-11 20:13:41 +0200

    change classNames to ascending

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -8,14 +8,14 @@ import Thumbnail from './Thumbnail'
 export default function Tweet({ author, date, content, imageURL }) {
   return (
     <div className="row p-3 border mb-2">
-      <div className="col-sm-2">
+      <div className="col-xs-12 col-sm-2">
         <Thumbnail imageURL={imageURL}/>
       </div>
-      <div className="col-sm-6">
+      <div className="col-xs-12 col-sm-6">
         <Author author={author}/>
         <Content content={content}/>
       </div>
-      <div className="col-sm-4">
+      <div className="col-xs-12 col-sm-4">
         <Date date={date}/>
       </div>
 
